43a45d95a70c170516392e49cf2b9dafd4676915
Momen Noor <momen.noor@gmail.com>
2021-03-20 16:38:41 +0400

    Update CountryNotFoundException.php

--- src/CountryNotFoundException.php
+++ src/CountryNotFoundException.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 namespace Monarobase\CountryList;
 
 /**
- * CountryNotFoundException
+ * This file is part of M-I-E-Systems\CountryList
+ * Reference : CountryList by Momenoor <momen.noor@gmail.com>
  *
- * @author Yohann Bianchi<yohann.b@lahautesociete.com>
- * @copyright 2015 La Haute Société - http://www.lahautesociete.com/
+ * (c) 2015-2021 M.I.E Systems
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ *
+ * @category    M-I-E-Systems
+ * @package     CountryList
+ * @copyright   (c) 2015-2021 M-I-E-Systems <momen.noor@gmail.com>
+ * @link        https://MIE.Systems
  */
+
 class CountryNotFoundException extends \Exception
 {
     /**
